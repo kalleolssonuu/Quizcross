1ece15a3d29e12fc59d4eca59494949b602dbc2c
Elinelin22 <elin.eckervald.4418@student.uu.se>
2022-12-12 19:20:59 +0100

    njut av min vackra stämma

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -16,6 +16,16 @@
     <button id="play" @click="$router.push('/play/'+lang)">{{uiLabels.playCross}}</button>
     <button id="play" @click="$router.push('/kalletest/'+lang)">{{'Göra korsord test'}}</button>
   </div>
+
+<audio src="01 Manboy.m4a" controls>
+  <embed
+  src="01 Manboy.m4a"
+  loop="true"
+  autostart="true"
+  height="0"
+  hidden>
+</audio>
+
 </template>
 
 <script>
