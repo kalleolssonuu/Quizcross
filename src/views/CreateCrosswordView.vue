fb9c3227360fff6e0ee5324c59e89640fef2253a
Huggman <hugo.ericssonlfc@gmail.com>
2022-12-19 17:38:32 +0100

    tyvärr måste kanske animeringen bort

--- src/views/CreateCrosswordView.vue
+++ src/views/CreateCrosswordView.vue
@@ -38,7 +38,7 @@
         <div id="div3">
         <!--<button v-on:click="this.emptyTextFields"> Empty Input </button> ---><!-- gör detta när användaren har valt ett ord istället för en knapp. Det rensar även textfältet -->
         <button v-on:click="this.fillPositionsNull"> Reset Crossword </button> <br>
-        <button class="glow-on-hover"> PLAY </button>
+        <button> PLAY </button>
         </div>
         <br>
 
@@ -470,65 +470,5 @@ button {
     height: 3rem;
     width: 8rem;
   }
-    /*här kommer en style för en FET button*/
-    .glow-on-hover {
-    width: 10rem;
-    height: 4rem;
-    border-radius: 20px;
-    border-color: #ba0c00;
-    outline: none;
-    color: #fff;
-    background: #FE5F55;
-    cursor: pointer;
-    position: relative;
-    z-index: 0;
-    border-radius: 10px;
-}
 
-.glow-on-hover:before {
-    content: '';
-    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
-    position: absolute;
-    top: -2px;
-    left:-2px;
-    background-size: 400%;
-    z-index: -1;
-    filter: blur(5px);
-    width: calc(100% + 4px);
-    height: calc(100% + 4px);
-    animation: glowing 20s linear infinite;
-    opacity: 0;
-    transition: opacity .3s ease-in-out;
-    border-radius: 10px;
-}
-
-.glow-on-hover:active {
-    color: #ffffff
-}
-
-.glow-on-hover:active:after {
-    background: transparent;
-}
-
-.glow-on-hover:hover:before {
-    opacity: 1;
-}
-
-.glow-on-hover:after {
-    z-index: -1;
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: #FE5F55;
-    left: 0;
-    top: 0;
-    border-radius: 10px;
-}
-
-@keyframes glowing {
-    0% { background-position: 0 0; }
-    50% { background-position: 400% 0; }
-    100% { background-position: 0 0; }
-}
 </style>
