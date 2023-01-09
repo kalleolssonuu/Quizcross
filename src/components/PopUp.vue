145f0b38bb9cd5fd1ba6c8f425da8adb86d68682
Elinelin22 <elin.eckervald.4418@student.uu.se>
2023-01-09 17:21:33 +0100

    popup klar, mobilversion

--- src/components/PopUp.vue
+++ src/components/PopUp.vue
@@ -76,36 +76,36 @@ export default {
   z-index: 9998;
   top: 0;
   left: 0;
-  width: 200vw;
-  height: 200vh;
+  width: 100%;
+  height: 100%;
   background-color: rgba(0, 0, 0, .5);
 }
 
 .modal-vue .modal {
   position: absolute;
-  top: 25%;
-  left: 27%;
-  height: 20%;
-  width: 40%;
+  top: 15vw;
+  left: 27vw;
+  height: 10vw;
+  width: 40vw;
   z-index: 9999;
   margin: 0 auto;
   padding: 20px 30px;
   background-color: #FFFDD0;
   color: black;
   border-radius: 15px;
-  font-family: "Comic Sans MS", "Comic Sans", cursive;
+  font-family: "Comic Sans MS", "Comic Sans";
   overflow-y: auto;
 }
 
 .modal-vue .close{
   position: absolute;
-  top: 10px;
-  right: 10px;
+  top: 1vw;
+  right: 1vw;
   background-color: #FE5F55;
   border-radius: 5px;
   cursor:pointer;
-  height: 1.8rem;
-  width: 1.8rem;
+  height: 2vw;
+  width: 2vw;
   color: black;
 }
 .modal-vue .close:hover{
@@ -128,6 +128,6 @@ background-color: #e36f67;
   color: white;
   /* padding-top:1vw; */
   /*text-align: center;*/
-  font-family: "Comic Sans MS", "Comic Sans", cursive;
+  font-family: "Comic Sans MS", "Comic Sans";
 }
 </style>
